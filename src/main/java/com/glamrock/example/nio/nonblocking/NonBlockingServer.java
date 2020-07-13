57d88acadb5d36908ad2788bd909bf3a69e2d458
glamrock <chivaries@gmail.com>
2020-07-13 13:56:16 +0900

    넌블러킹 채팅 서버, 클라이언트 수정 완료

--- src/main/java/com/glamrock/example/nio/nonblocking/NonBlockingServer.java
+++ src/main/java/com/glamrock/example/nio/nonblocking/NonBlockingServer.java
@@ -9,14 +9,11 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.Vector;
+import java.util.*;
 
 public class NonBlockingServer {
     Selector selector;
-    private Vector<SocketChannel> rooms = new Vector();
-    private CharsetDecoder decoder = null;
+    private List<Client> rooms = new ArrayList<>();
 
     public void initServer() throws IOException {
         selector = Selector.open();
@@ -26,9 +23,6 @@ public class NonBlockingServer {
         // Selector 에 accept 작업 등록
         System.out.println(String.format("[%s] Selector 에 accept 작업 등록 ", Thread.currentThread().getName()));
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-
-        Charset charset = Charset.forName("UTF-8");
-        decoder = charset.newDecoder();
     }
 
     public void startServer() {
@@ -60,9 +54,11 @@ public class NonBlockingServer {
                         if (selectionKey.isAcceptable()) {
                             accept(selectionKey);
                         } else if (selectionKey.isReadable()) {
-                            read(selectionKey);
+                            Client client = (Client)selectionKey.attachment();
+                            client.receive(selectionKey);
                         } else if (selectionKey.isWritable()) {
-                            /* 쓰기 작업 처리 */
+                            Client client = (Client)selectionKey.attachment();
+                            client.send(selectionKey);
                         }
 
                         iterator.remove();
@@ -82,87 +78,12 @@ public class NonBlockingServer {
         // 서버소켓 accept() 메소드로 서버소켓을 생성한다.
         SocketChannel socketChannel = server.accept();
 
-        if (socketChannel == null)
-            return;
+        if (socketChannel == null) return;
 
         System.out.println(String.format("[%s] %s 클라이언트 접속 (SocketChannel 생성) ", Thread.currentThread().getName(), socketChannel.toString()));
 
-        socketChannel.configureBlocking(false);
-        socketChannel.register(selector, SelectionKey.OP_READ);
-        System.out.println(String.format("[%s] %s Selector 에 read 작업 등록 ", Thread.currentThread().getName(), socketChannel.toString()));
-
-        rooms.add(socketChannel);
+        rooms.add(new Client(socketChannel));
     }
-
-    private void read(SelectionKey selectionKey) {
-        // SelectionKey 로부터 소켓채널을 얻는다.
-        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
-
-        try {
-            socketChannel.read(byteBuffer);
-            System.out.println(String.format("[%s] %s 데이터 읽기 ", Thread.currentThread().getName(), socketChannel.toString()));
-        } catch (IOException ex) {
-            try {
-                socketChannel.close();
-            } catch(IOException e) {
-                e.printStackTrace();
-            }
-
-            rooms.remove(socketChannel);
-            ex.printStackTrace();
-        }
-        
-        try {
-            broadcast(byteBuffer);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        byteBuffer.clear();
-    }
-
-    private void broadcast(ByteBuffer byteBuffer) throws IOException {
-        byteBuffer.flip();
-        Iterator iterator = rooms.iterator();
-
-        String data = decoder.decode(byteBuffer).toString();
-        System.out.println(String.format("[%s] 메세지 받음 - %s", Thread.currentThread().getName(), data));
-        byteBuffer.rewind();
-
-        while (iterator.hasNext()) {
-            SocketChannel socketChannel = (SocketChannel) iterator.next();
-
-            if (socketChannel != null) {
-//                SelectionKey selectionKey = socketChannel.keyFor(selector);
-//                selectionKey.interestOps(SelectionKey.OP_WRITE);
-                System.out.println(String.format("[%s] %s 클라이언트로 메세지 전송", Thread.currentThread().getName(), socketChannel.toString()));
-                socketChannel.write(byteBuffer);
-                byteBuffer.rewind();
-            }
-        }
-    }
-
-//    void send(SelectionKey selectionKey) {
-//        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-//
-//        try {
-//            Charset charset = Charset.forName("UTF-8");
-//            ByteBuffer byteBuffer = charset.encode(sendData);
-//
-//            socketChannel.write(byteBuffer);
-//            selectionKey.interestOps(SelectionKey.OP_READ);
-//            selector.wakeup();
-//        } catch (Exception e) {
-//            try {
-//                rooms.remove(socketChannel);
-//                socketChannel.close();
-//            } catch (IOException iox) {
-//
-//            }
-//        }
-//    }
 
     public static void main(String[] args) {
         NonBlockingServer nonBlockingServer = new NonBlockingServer();
@@ -174,5 +95,83 @@ public class NonBlockingServer {
             e.printStackTrace();
         }
 
+    }
+
+    public class Client {
+        SocketChannel socketChannel;
+        String sendData;
+
+        Client(SocketChannel socketChannel) throws IOException {
+            this.socketChannel = socketChannel;
+
+            socketChannel.configureBlocking(false);
+            SelectionKey selectionKey = socketChannel.register(selector, SelectionKey.OP_READ);
+            selectionKey.attach(this);
+
+            System.out.println(String.format("[%s] %s Selector 에 read 작업 등록 ", Thread.currentThread().getName(), socketChannel.toString()));
+        }
+
+        void receive(SelectionKey selectionKey) {
+
+            try {
+                ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+                int byteCount = socketChannel.read(byteBuffer);
+
+                if (byteCount == -1) {
+                    throw new IOException();
+                }
+
+                byteBuffer.flip();
+                Charset charset = Charset.forName("UTF-8");
+                String data = charset.decode(byteBuffer).toString();
+
+                System.out.println(String.format("[%s] %s 데이터 읽기 : %s ", Thread.currentThread().getName(), socketChannel.toString(), data));
+
+                rooms.stream()
+                        .filter(client -> !client.socketChannel.equals(socketChannel))
+                        .forEach(client -> {
+                            client.sendData = data;
+                            SelectionKey key = client.socketChannel.keyFor(selector);
+                            key.interestOps(SelectionKey.OP_WRITE);
+                            System.out.println(String.format("[%s] %s Selector 에 write 작업 등록 ", Thread.currentThread().getName(), client.socketChannel.toString()));
+                        });
+
+                selector.wakeup();
+                System.out.println(String.format("[%s] %s selector.wakeup() 호출 ", Thread.currentThread().getName(), socketChannel.toString()));
+            } catch (IOException e) {
+                try {
+                    rooms.remove(this);
+                    System.out.println(String.format("[%s] 클라이언트 통신 안됨 : %s]", Thread.currentThread().getName() , socketChannel.toString()));
+                    socketChannel.close();
+                } catch (IOException e2) {
+
+                }
+            }
+
+        }
+
+        void send(SelectionKey selectionKey) {
+            try {
+                Charset charset = Charset.forName("UTF-8");
+                ByteBuffer byteBuffer = charset.encode(sendData);
+
+                socketChannel.write(byteBuffer);
+                System.out.println(String.format("[%s] %s 데이터 쓰기 : %s ", Thread.currentThread().getName(), socketChannel.toString(), sendData));
+
+                selectionKey.interestOps(SelectionKey.OP_READ);
+                System.out.println(String.format("[%s] %s Selector 에 read 작업 등록 ", Thread.currentThread().getName(), socketChannel.toString()));
+
+                selector.wakeup();
+                System.out.println(String.format("[%s] %s selector.wakeup() 호출 ", Thread.currentThread().getName(), socketChannel.toString()));
+            } catch (Exception e) {
+                try {
+                    rooms.remove(this);
+                    System.out.println(String.format("[%s] 클라이언트 통신 안됨 : %s]", Thread.currentThread().getName() , socketChannel.toString()));
+                    socketChannel.close();
+                } catch (IOException e2) {
+
+                }
+            }
+        }
     }
 }
